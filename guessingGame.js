101f71ad69473c857910c72828a11e372b6453a5
John Rauschenberg <john.rauschenberg@gmail.com>
2015-12-10 22:31:51 -0500

    Began adding functionality to guessing game (checking guess).

--- guessingGame.js
+++ guessingGame.js
@@ -1,35 +1,57 @@
 /* **** Global Variables **** */
 // try to elminate these global variables in your project, these are here just to start.
 
-var playersGuess,
-    winningNumber
-
-
+var playersGuess
+var totalGuesses = 5;
+var pastGuesses = [];
+var winningNumber = generateWinningNumber();
 
 /* **** Guessing Game Functions **** */
 
 // Generate the Winning Number
 
 function generateWinningNumber(){
-	// add code here
+  return Math.floor(Math.random() * 100);
 }
 
 // Fetch the Players Guess
 
 function playersGuessSubmission(){
-	// add code here
+	playersGuess = Number($('#guess').val());
+  $('#guess').val("");
+  checkGuess();
 }
 
 // Determine if the next guess should be a lower or higher number
 
 function lowerOrHigher(){
-	// add code here
+	
+}
+
+function guessMessage(){
+  
 }
 
 // Check if the Player's Guess is the winning number 
 
 function checkGuess(){
-	// add code here
+  for (var i=0; i<pastGuesses.length; i++) {
+    if (pastGuesses[i] === playersGuess) {
+      $('#result').text("You already guessed that number!");
+      $('#result').css("color", "red");
+      return;
+    }
+  }
+  pastGuesses.push(playersGuess);
+  totalGuesses -= 1;
+	if (playersGuess === winningNumber) {
+    $('#result').text("You won!");
+    $('#result').css("color", "green");
+  }
+  else {
+    $('#result').text("Try again. You have " + totalGuesses + " guesses remaining.");
+    $('#result').css("color", "red");
+  }
 }
 
 // Create a provide hint button that provides additional clues to the "Player"
